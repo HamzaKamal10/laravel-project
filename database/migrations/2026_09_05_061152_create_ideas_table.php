43c94b5457a6b668c9d17382838f32ba895ea006
hamza <almodwahih@gmail.com>
2026-09-05 13:10:08 +0300

    Migration: update ideas table to include user foreignId and requested fields

--- database/migrations/2026_09_05_061152_create_ideas_table.php
+++ database/migrations/2026_09_05_061152_create_ideas_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -12,12 +13,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('ideas', function (Blueprint $table) {
-            $table->id(); // المعرف الفريد للفكرة
-            $table->string('title'); // عنوان الفكرة (نص قصير)
-            $table->text('description'); // تفاصيل الفكرة (نص طويل)
-            $table->string('status')->default('Pending'); // حالة الفكرة (الافتراضي: قيد الانتظار)
-            $table->timestamps(); // تاريخ الإنشاء والتحديث
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('status')->default('pending');
+            $table->string('image_path')->nullable();
+            $table->json('links')->default('[]');
+            $table->timestamps();
         });
+
+        
     }
 
     /**
