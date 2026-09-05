f4e3c9d22a7ceba3bae2a2465b603e6b63a727d0
hamza <almodwahih@gmail.com>
2026-09-05 13:27:15 +0300

    Fix: import Eloquent Model in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Model;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+      Model::unguard();
+      Model::shouldBeStrict();
+      Model::automaticallyEagerLoadRelationships();
     }
 }
